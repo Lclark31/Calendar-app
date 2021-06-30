b073b4c31829aa260fa17aafbb49ee9b9d9144bd
Lashaun <clarklas831@gmail.com>
2021-06-29 20:23:35 -0500

    html layout

---
+++ script.js
@@ -0,0 +1,14 @@
+// display current day on top
+// moment.js to recognize current time
+
+// color-code time blocks for if they're present, future, or past
+// change class attributes dependent on current time
+
+// time blocks are text areas that can be clicked and edited
+// hmm..
+
+// when saved button is clicked, event is saved in localStorage
+// saveButton.on(`click` localStorage.setItem)
+
+// when page is refreshed events persist
+// hmm..
