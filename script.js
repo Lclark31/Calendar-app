3ec54fc5d2cf45683f4710df1047300f5a03e84b
Lashaun <clarklas831@gmail.com>
2021-07-03 13:17:56 -0500

    save events

--- script.js
+++ script.js
@@ -4,27 +4,23 @@ let inputEl;
 let newEventEl;
 let eventArray = [];
 let timesArray = [];
+
+// compare each time to the current time and adjust classes accordingly
 // color-code time blocks for if they're present, future, or past
 // change class attributes dependent on current time
-
 function updateTime() {
-  $(`#currentDay`).text(moment().format(`dddd, MMMM Do YYYY mm:ss`));
+  $(`#currentDay`).text(moment().format(`dddd, MMMM Do YYYY`));
   timeEl = $(`.hour`)[0];
-  // console.log(timeEl.innerHTML);
   for (i = 0; i < $(`.hour`).length; i++) {
     time = $(`.hour`)[i].innerHTML;
     timesArray.push(time);
     localStorage.setItem(`Times`, JSON.stringify(timesArray));
   }
   timesArray = JSON.parse(localStorage.getItem(`Times`));
-  time = moment(timesArray[4], `hA`);
-  console.log(timesArray[4]);
-  console.log(moment().format(`hA`));
+  time = moment(timesArray[1], `hA`);
   if (moment().isAfter(time, `hour`)) {
-    $(`.10AM`).addClass(`present`);
+    $(`.9AM`).addClass(`present`);
   } else if (time.isAfter(moment())) {
-    console.log(`true`);
-    $(`.10AM`).addClass(`future`);
   }
 }
 
@@ -35,7 +31,7 @@ function editEvent() {
   inputEl = $(`<textarea>`).addClass(`description col-10`).val(text);
 
   $(this).replaceWith(inputEl);
-  inputEl.focus();
+  inputEl.focus().select();
 
   $(document).ready(function () {
     $(`textarea`).blur(function () {
@@ -45,21 +41,36 @@ function editEvent() {
 }
 
 function blurredEvent() {
+  $(`.icon`).removeClass(`fa-lock`);
+  $(`.icon`).addClass(`fa-lock-open`);
+
   newEventEl = $(`<div>`).addClass(`col-10 past description `);
   newEventEl.text($(`textarea`).val());
 
   $(`textarea`).replaceWith(newEventEl);
-  saveEvent();
   $(newEventEl).on(`click`, editEvent);
 }
 
-// when saved button is clicked, event is saved in localStorage
-// saveButton.on(`click` localStorage.setItem)
 function saveEvent() {
-  return;
+  $(event.target).addClass(`fa-lock`);
+  $(event.target).removeClass(`fa-lock-open`);
+  eventArray = [];
+
+  for (i = 0; i < $(`.description`).length; i++) {
+    eventArray.push($(`.description`)[i].innerHTML);
+  }
+  localStorage.setItem(`Events`, JSON.stringify(eventArray));
 }
 
-// when page is refreshed events persist
-// hmm..
+function pullEvents() {
+  eventArray = JSON.parse(localStorage.getItem(`Events`));
 
+  for (i = 0; i, i < eventArray.length; i++) {
+    let oldEvents = calendarEventEl[i];
+    oldEvents.innerHTML = eventArray[i];
+  }
+}
+
+$(window).on(`load`, pullEvents);
 $(calendarEventEl).on(`click`, editEvent);
+$(`.icon`).on(`click`, saveEvent);
